73615700d579c69b24593985fa9940d9dea0f3cd
Fayaz K <code@fayazk.com>
2024-02-26 23:52:53 +0500

    🎨 style(app.css): add margin utility classes for better layout control 🔨 refactor(VerifyEmail.jsx): switch from GuestLayout to AuthLayout for better UX ✨ feat(VerifyEmail.jsx): add form validation and error handling for email verification 🔧 fix(VerifyEmail.jsx): replace PrimaryButton with antd Button for consistency in UI

--- resources/js/Pages/Auth/VerifyEmail.jsx
+++ resources/js/Pages/Auth/VerifyEmail.jsx
@@ -1,45 +1,58 @@
-import GuestLayout from '@/Layouts/GuestLayout';
-import PrimaryButton from '@/Components/PrimaryButton';
-import { Head, Link, useForm } from '@inertiajs/react';
+import {  Link, router } from '@inertiajs/react'
+import AuthLayout from '@/Layouts/AuthLayout.jsx'
+import { Alert, Button, Flex, Form, Typography } from 'antd'
+import { useState } from 'react'
 
-export default function VerifyEmail({ status }) {
-    const { post, processing } = useForm({});
+export default function VerifyEmail() {
+    const [form] = Form.useForm();
+    const [status, setStatus] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-    const submit = (e) => {
-        e.preventDefault();
-
-        post(route('verification.send'));
-    };
+    const submit = (values) => {
+        router.post(route('verification.send'),values,{
+            onSuccess:(props)=>{
+                setStatus(props.props.status)
+            },
+            onError: (errors) => {
+                console.error(errors)
+            },
+            onFinish: () => {
+                setLoading(false)
+            },
+        })
+    }// submit
 
     return (
-        <GuestLayout>
-            <Head title="Email Verification" />
+        <AuthLayout title={'Email Verification'}>
 
-            <div className="mb-4 text-sm text-gray-600">
+            <Typography.Paragraph >
                 Thanks for signing up! Before getting started, could you verify your email address by clicking on the
                 link we just emailed to you? If you didn't receive the email, we will gladly send you another.
-            </div>
+            </Typography.Paragraph>
 
             {status === 'verification-link-sent' && (
-                <div className="mb-4 font-medium text-sm text-green-600">
+                <Alert type={'success'} closable={true} className={'mt-1 mb-1'}>
                     A new verification link has been sent to the email address you provided during registration.
-                </div>
+                </Alert>
             )}
 
-            <form onSubmit={submit}>
-                <div className="mt-4 flex items-center justify-between">
-                    <PrimaryButton disabled={processing}>Resend Verification Email</PrimaryButton>
-
+            <Form onFinish={submit} form={form}>
+                <Form.Item>
+                    <Button type={'primary'} htmlType={'submit'} block={true} loading={loading}>
+                        Resend Verification Email
+                    </Button>
+                </Form.Item>
+                <Flex justify={'space-around'}>
                     <Link
-                        href={route('logout')}
-                        method="post"
-                        as="button"
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      href={route('logout')}
+                      method="post"
+                      as="button"
+                      className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                         Log Out
                     </Link>
-                </div>
-            </form>
-        </GuestLayout>
+                </Flex>
+            </Form>
+        </AuthLayout>
     );
 }
