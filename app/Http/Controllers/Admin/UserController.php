0bc56a697613d08859120515b7999929543b0b82
Fayaz K <code@fayazk.com>
2024-03-08 01:53:30 +0500

    📝 docs(UserController.php): add method descriptions for better code readability 🎨 style(UserController.php): adjust code formatting for consistency

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -9,15 +9,23 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-
+    /**
+     * Display User Index Page
+     *
+     * @return \Inertia\Response
+     */
     public function index()
     {
-        return inertia('Admin/Users/Index');
-    }// index
+        return inertia("Admin/Users/Index");
+    } // index
 
+    /**
+     * Get All Users
+     *
+     * @return UserCollection
+     */
     public function all()
     {
         return new UserCollection(User::paginate(25));
-    }// all
-
-}// UserController
+    } // all
+} // UserController
